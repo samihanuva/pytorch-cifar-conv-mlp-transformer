f72bc46bd6827c4e19ed2dab158e70b372959723
samihanuva <62112254+samihanuva@users.noreply.github.com>
2022-11-01 22:24:57 +0600

    Update train_cifar10.py

--- train_cifar10.py
+++ train_cifar10.py
@@ -96,7 +96,7 @@ if aug:
 
 # Prepare dataset
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=True, num_workers=8)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=False, num_workers=8)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
