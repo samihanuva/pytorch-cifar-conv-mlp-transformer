e2648e69f3ae7932697113fb4bfe711cb43ebfd9
samihanuva <62112254+samihanuva@users.noreply.github.com>
2022-11-01 23:30:09 +0600

    seed cuda and os

--- train_cifar10.py
+++ train_cifar10.py
@@ -36,7 +36,8 @@ torch.manual_seed(37)
 random.seed(37)
 np.random.seed(37)
 torch.use_deterministic_algorithms(True)
-
+torch.cuda.manual_seed_all(37)
+os.environ["CUBLAS_WORKSPACE_CONFIG"]=":4096:8"
 
 
 
