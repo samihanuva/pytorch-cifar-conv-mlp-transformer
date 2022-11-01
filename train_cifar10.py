6b59198ea86117fafea6f4043ec4ad8c1e9bc745
samihanuva <62112254+samihanuva@users.noreply.github.com>
2022-11-01 22:26:48 +0600

    shuffle set to false for train set

--- train_cifar10.py
+++ train_cifar10.py
@@ -96,6 +96,7 @@ if aug:
 
 # Prepare dataset
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+#shuffle set to false 
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=False, num_workers=8)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
